bf8649c5cb0d732c74fb1d6ac8318983415005c9
CodeScript990 <trte135@naver.com>
2023-06-12 16:43:24 +0900

    feat.June.12.23 [Algorithms - Stack]  - Content :    - Stack    - ParTest[brackets question]    - ParTest2[brackets,braces, square brackets question]

---
+++ mvc/src/main/java/com/example/mvc/algorithms/day2/ParTest2.java
@@ -0,0 +1,46 @@
+package com.example.mvc.algorithms.day2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class ParTest2 {
+    public boolean solution() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input = reader.readLine(); // 괄호 입력 받는 부분
+        Stack<Character> charStack = new Stack<>();// Stack 형태 Character type, charStack 참조변수
+
+        // 소괄호, 중괄호, 대괄호를 이루어진 수식을 검사하는 코드를 작성하시오.
+        for (int i = 0; i < input.length(); i++) {
+
+            char next = input.charAt(i);
+
+            // 1. 여는 소괄호를 만날때 push
+            if (next == '(' || next == '{' || next == '[') {
+                charStack.push(next);
+            }
+            // 2. 닫는 소괄호를 만날때 pop
+            else if (next == ')' || next == '}' || next == ']') {
+                // pop 할게 없으면 검사 실패를 한다
+                if (charStack.empty()) return false;
+
+                // 아니라면 pop을 한다
+                char top = charStack.pop();
+
+                // pop의 결과로 나온 값이 올바른 여는 괄호인지 확인한다
+                // 들오온건 닫는 소괄호 && top은 여는 소괄호가 아닐 때 실패한다
+                if (next == ')' && top != '(') return false; // 소괄호
+                else if (next == '}' && top != '{') return false; // 중괄호
+                else if (next == ']' && top != '[') return false; // 대괄호
+            }
+        }
+        // 순회가 끝났다면 Stack이 비었는지 확인해준다
+        return charStack.empty();
+    }
+
+    // main method => test
+    public static void main(String[] args) throws IOException {
+        System.out.println(new ParTest2().solution());
+    }
+}
---
+++ mvc/src/main/java/com/example/mvc/algorithms/day2/Stack.java
@@ -0,0 +1,84 @@
+package com.example.mvc.algorithms.day2;
+
+// Stack
+public class Stack {
+    // Stack에 실제 Data가 지정되는 곳[Field]
+    private final int[] arr = new int[16]; // final : 주소를 바꿀 수 있는가 없는가, 어디서 시작되는가를 말하는 것
+    // 현재 Statck의 최고점 파악하기 위한 top[Field]
+    private int top = -1;
+    // Empty Constructor
+    public Stack() { }
+
+    // push : Data를 Stack의 제일 위에 넣는 Method
+    public void push(int data) {
+        // 0. arr가 가득 찾는지를 판단한다
+        if (arr.length - 1 == top) {
+            throw new RuntimeException("Stack is full");
+        }
+
+        // 1. top을 하나 증가 시킵니다.
+        top++;
+
+        // 2. arr[top]를 할당한다.
+        arr[top] = data;
+
+        // 3. Done!
+        // arr[++top] = data;
+    }
+
+    // pop : Data를 Stack의 제일 위에서 회수하는 Method
+    public int pop() {
+        // 0. Arr가 비어있는지 판단합니다 [기준 => top]
+        if (empty()) {
+            throw new RuntimeException("Stack is empty");
+        }
+
+        // 1. Arr[top]의 값을 반환합니다
+        int  temp = arr[top];
+
+        // 2. top의 값을 하나 감소합니다
+        top--;
+
+        // 3. Return temporary value
+        return temp;
+
+        // 4. Done!
+        // return arr[top--];
+    }
+
+    // Peek : Return the top of stack without removing
+    // without removing => do not change too
+    public int peek() {
+        // 0. Check if stack not empty
+        if (empty()) {
+            throw new RuntimeException("Stack is empty");
+        }
+
+        // 1. return arr top
+        return arr[top];
+    }
+
+    // Empty : Check if stack is empty
+    public boolean empty() {
+        // stack is empty when top == -1
+        return top == -1;
+    }
+
+    // main method => Test
+    public static void main(String[] args) {
+        // Stack 3개의 Data를 넣었다가 빼보기
+        Stack stack = new Stack();
+
+        // push
+        stack.push(3);
+        stack.push(5);
+        stack.push(7);
+
+        // stack => 3(0), 5(1), 7(2)
+        System.out.printf("Stack Pop : %d\n", stack.pop());
+        System.out.printf("Stack Pop : %d\n", stack.pop());
+        System.out.printf("Stack Empty : %b\n", stack.empty());
+        System.out.printf("Stack Peek : %d\n", stack.peek());
+        System.out.printf("Stack Pop : %d\n", stack.pop());
+    }
+}
