1fee6771769caecaa030a6a97c80d5e08907579f
CodeScript990 <trte135@naver.com>
2023-06-13 17:59:19 +0900

    feat.June.13.23 [Algorithms - Stack2]  - Content :    - DepthFirstSearch[DFS - 깊이 우선 탐색]    - InfixToPostfix[중위 표기법]    - PostfixCalculation[후위 표기법]

---
+++ mvc/src/main/java/com/example/mvc/algorithms/day3/DepthFirstSearch.java
@@ -0,0 +1,61 @@
+package com.example.mvc.algorithms.day3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Stack;
+
+// 깊이 우선 탐색[DFS]
+public class DepthFirstSearch {
+    // solution method
+    public void solution() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // 첫 입력은 정점의 갯수
+        int maxNodes = Integer.parseInt(reader.readLine());
+        
+        // array
+        // 정점간 연결 정보
+        int[][] edgeMap = new int[maxNodes + 1][maxNodes + 1];
+        
+        // 1 2 1 3 2 4 2 5 4 6 5 6 6 7 3 7
+        String[] edges = reader.readLine().split(" ");
+        // for statement => 2개씩 순회
+        for (int i = 0; i < edges.length / 2; i++) {
+            int leftNode = Integer.parseInt(edges[i * 2]); // 0, 2, 4, ...
+            int rightNode = Integer.parseInt(edges[i * 2 + 1]); // 1, 3, 5, ...
+            
+            edgeMap[leftNode][rightNode] = 1;
+            edgeMap[rightNode][leftNode] = 1;
+        }
+        
+        // 다음에 방문할 점들을 담아두는 Stack
+        Stack<Integer> toVisit = new Stack<>();
+        
+        // 방문을 기록하는 용도의 배열
+        boolean[] visited = new boolean[maxNodes + 1];
+
+        // DFS
+
+        // 첫 방문 대상 신청(1)
+        int next = 1;
+        // 대상을 Stack push
+        toVisit.push(next);
+        // 스택이 빌때까지 반복하는 while
+        while (!toVisit.empty()) {
+            // TODO 다음 방문할 곳을 가져온다
+            // TODO 이미 방문했다면 다음 곳으로 간다(pop)
+            // TODO 방문했다 표시한다
+            // TODO 요부분은 문제에 따라 다르다
+            // TODO 다음 방문 대상을 검색해서, 스택에 푸시 한다
+            for (;;) { // TODO 그래프에 존재하는 점들을 순회
+                // TODO 해당 장점에 도달할 수 있고, 아직 방문하지 않았다면
+                    // TODO 스택에 푸시
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new DepthFirstSearch().solution();
+    }
+}
---
+++ mvc/src/main/java/com/example/mvc/algorithms/day3/PostfixCalculation.java
@@ -0,0 +1,63 @@
+package com.example.mvc.algorithms.day3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+// Reverse Polish Notation -> Postfix Notation
+// 후위 표기법
+public class PostfixCalculation {
+
+    // solution method
+    public void solution() throws IOException {
+        // 입력 처리
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // 입력 받기
+        String input = reader.readLine();
+        // Stack 형태 Integer type
+        Stack<Integer> digitStack = new Stack<>();
+
+        // for statement
+        char token = 0;
+        for (int i = 0; i < input.length(); i++) {
+            // token char type
+            token = input.charAt(i);
+
+            // 1. 숫자라면, Stack 에 push 한다
+            // Character.isDigit(token) // token 의 숫자가 표현된 글자인지 판단하는 Method
+            // Token 을 int 로 변환 => token - '0'
+            if (Character.isDigit(token)) { // token 의 숫자가 표현된 글자인지 판단하는 Method
+                // 숫자니깐 변환 후 push
+                digitStack.push(Character.digit(token, 10));
+            }
+
+            // 2. 숫자가 아니라면,
+            else {
+                // (연산자) Stack 에서 2번 pop 하고 계산한다
+                int numRight = digitStack.pop();
+                int numLeft = digitStack.pop();
+
+                // switch statement
+                switch (token) {
+                    case '+' -> digitStack.push(numLeft + numRight);
+                    case '-' -> digitStack.push(numLeft - numRight);
+                    case '*' -> digitStack.push(numLeft * numRight);
+                    case '/' -> digitStack.push(numLeft / numRight);
+                    default -> throw new IllegalArgumentException("invalid operator");
+                }
+            }
+        }
+        // 마지막 연산 결과
+//        System.out.println(digitStack.pop());
+        // answer value => pop[Result Value == last value]
+        int answer = digitStack.pop();
+        // if statement => true 이면 answer
+        if(digitStack.empty()) System.out.println(answer);
+        // else statement => false 이면 error message
+        else System.out.println("error");
+    }
+    public static void main(String[] args) throws IOException {
+        new PostfixCalculation().solution();
+    }
+}
