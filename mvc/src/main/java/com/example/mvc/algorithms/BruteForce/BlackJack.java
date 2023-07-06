b968d6293226772a6d7654af950eed160d4e023c
CodeScript990 <trte135@naver.com>
2023-07-06 21:21:33 +0900

    feat.July.5.23 [Algorithms - BruteForce, 백준문제 풀이]  - Content :    - BlackJack[블랙잭 - 백준알고리즘 풀이]    - Permutation[순열 문제(재귀함수를 이용하여) 풀이]    - Combination[조합 - 백준알고리즘 풀이]    - NumberBaseball[숫자야구 - 백준알고리즘 풀이]

---
+++ mvc/src/main/java/com/example/mvc/algorithms/BruteForce/BlackJack.java
@@ -0,0 +1,5 @@
+package com.example.mvc.algorithms.BruteForce;
+
+public class BlackJack {
+
+}
--- mvc/src/main/java/com/example/mvc/algorithms/BruteForce/Permutation.java
+++ mvc/src/main/java/com/example/mvc/algorithms/BruteForce/Permutation.java
@@ -1,5 +1,7 @@
 package com.example.mvc.algorithms.BruteForce;
 
+import java.util.Arrays;
+
 public class Permutation {
     public static void main(String[] args) {
         System.out.println("--- 3! ---");
@@ -61,8 +63,85 @@ public class Permutation {
                 if (i == j) continue;
                 for (int k = 1; k < 10; k++) {
                     if (k == i || k == j) continue;
+
                     System.out.printf("%d %d %d\n", i, j, k);
                 }
+            }
+        }
+        Permutation perm = new Permutation();
+        System.out.println("---- permNumbers ----");
+        perm.permNumbers(
+                5, 3, 0,
+                // 고른 숫자가 담기는 배열이기 때문에, r 의 크기를 갖는 배열
+                new int[3],
+                // 고를 수 있는 숫자가 골라졌는지 판단하는 배열이기 때문에, n의 크기를 갖는 배열
+                new boolean[5]);
+        System.out.println("---- permArray ----");
+        perm.permArray(new int[]{2,3,5,7,11}, 3, 0, new int[3], new boolean[5]);
+    }
+
+    // permNumbers method
+    private void permNumbers(
+            // 0 - n 까지 숫자 중
+            int n,
+            // r 개를 뽑아서 나열
+            int r,
+            // 몇번째 뽑는건지(재귀 조건)
+            int k,
+            // 여때까지 만든 순열
+            int[] perm,
+            // 사용여부를 확인하는 것
+            boolean[] used
+    ) {
+        // 선택할 만큼 선택함
+        if (k == r) {
+            System.out.println(Arrays.toString(perm));
+        }
+        else {
+            // 선택 가능한 숫자 다 확인
+            for (int i = 0; i < n; i++) {
+                // 이미 사용한 숫자인지 확인
+                if (used[i]) continue;
+                // 이번 숫자를 선택해서 저장
+                perm[k] = i;
+                used[i] = true;
+                // 다음 숫자 정하기 위한 재귀 호출
+                this.permNumbers(n, r, k + 1, perm, used);
+                // i를 k의 위치에 담는 과정을 마무리 했으면 i를 다시 사용가능한 상태로 만듬
+                used[i] = false;
+            }
+        }
+    }
+
+    // permArray method
+    private void permArray(
+            int[] numbers,
+            // r 개를 뽑아서 나열
+            int r,
+            // 이번에 몇번째 것을 뽑는건지 (재귀 조건)
+            int k,
+            // 여태까지 만든 순열
+            int[] perm,
+            // 이 숫자를 사용한적 있는지 여부에 대한 used
+            boolean[] used
+    ) {
+        // 선택할 만큼 선택했다.
+        if (k == r) {
+            System.out.println(Arrays.toString(perm));
+        }
+        else {
+            // 선택 가능한 숫자 다 확인
+            for (int i = 0; i < numbers.length; i++) {
+                // 이미 사용한 숫자인지 확인
+                if (used[i]) continue;
+                // 이번 숫자를 선택해서 저장
+                perm[k] = numbers[i];
+                used[i] = true;
+                // 다음 숫자 정하기 위한 재귀 호출
+                this.permArray(numbers, r, k + 1, perm, used);
+                // i를 k의 위치에 담는 과정을 마무리 했으면
+                // i를 다시 사용가능한 상태로
+                used[i] = false;
             }
         }
     }
